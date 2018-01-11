4691140cf5bd72c331a969b4e1aa77ec272e768b
daniellopezmorales <da.lopezm5@gmail.com>
2018-01-11 18:28:16 +0100

    Listado de tareas
    Listado de tareas

---
+++ platforms/browser/www/js/my-app.js
@@ -0,0 +1,104 @@
+// Initialize app
+var myApp = new Framework7();
+
+
+// If we need to use custom DOM library, let's save it to $$ variable:
+var $$ = Dom7;
+
+// Add view
+var mainView = myApp.addView('.view-main', {
+    // Because we want to use dynamic navbar, we need to enable it for this view:
+    dynamicNavbar: true
+});
+
+// Handle Cordova Device Ready Event
+$$(document).on('deviceready', function() {
+    console.log("Device is ready!");
+});
+
+
+// Now we need to run the code that will be executed only for About page.
+
+// Option 1. Using page callback for page (for "about" page in this case) (recommended way):
+myApp.onPageInit('about', function (page) {
+    // Do something here for "about" page
+
+})
+/*
+// Option 2. Using one 'pageInit' event handler for all pages:
+$$(document).on('pageInit', function (e) {
+    // Get page data from event data
+    var page = e.detail.page;
+
+    if (page.name === 'about') {
+        // Following code will be executed for page with data-page attribute equal to "about"
+        myApp.alert('Bienvenido a la página de contacto!');
+    }
+})*/
+/*
+// Option 2. Using live 'pageInit' event handlers for each page
+$$(document).on('pageInit', '.page[data-page="about"]', function (e) {
+    // Following code will be executed for page with data-page attribute equal to "about"
+    myApp.alert('Bienvenido a la página de contacto');
+})*/
+
+(function(){
+	// Variables
+	var lista = document.getElementById("lista"),
+		tareaInput = document.getElementById("tareaInput"),
+		btnNuevaTarea = document.getElementById("btn-agregar");
+
+	// Funciones
+	var agregarTarea = function(){
+		var tarea = tareaInput.value,
+			nuevaTarea = document.createElement("li"),
+			enlace = document.createElement("a"),
+			contenido = document.createTextNode(tarea);
+      myApp.alert('Nueva tarea añadida!');
+
+		if (tarea === "") {
+			tareaInput.setAttribute("placeholder", "Agrega una tarea valida");
+			tareaInput.className = "error";
+			return false;
+		}
+
+		// Agregamos el contenido al enlace
+		enlace.appendChild(contenido);
+		// Le establecemos un atributo href
+		enlace.setAttribute("href", "#");
+		// Agrergamos el enlace (a) a la nueva tarea (li)
+		nuevaTarea.appendChild(enlace);
+		// Agregamos la nueva tarea a la lista
+		lista.appendChild(nuevaTarea);
+
+		tareaInput.value = "";
+
+		for (var i = 0; i <= lista.children.length -1; i++) {
+			lista.children[i].addEventListener("click", function(){
+				this.parentNode.removeChild(this);
+			});
+		}
+
+	};
+	var comprobarInput = function(){
+		tareaInput.className = "";
+		teareaInput.setAttribute("placeholder", "Agrega tu tarea");
+	};
+
+	var eleminarTarea = function(){
+		this.parentNode.removeChild(this);
+	};
+
+	// Eventos
+
+	// Agregar Tarea
+	btnNuevaTarea.addEventListener("click", agregarTarea);
+
+	// Comprobar Input
+	tareaInput.addEventListener("click", comprobarInput);
+
+	// Borrando Elementos de la lista
+	for (var i = 0; i <= lista.children.length -1; i++) {
+		lista.children[i].addEventListener("click", eleminarTarea);
+	}
+}());
